fedad3ed90056dd20a2ae019f7b2b2cf09854046
kimsssang <swk9871@gmail.com>
2024-07-30 15:21:45 +0900

    07/30

---
+++ mingle/src/main/java/com/kh/common/JDBCTemplate.java
@@ -0,0 +1,14 @@
+package com.kh.common;
+
+import java.sql.Connection;
+import java.util.Properties;
+
+public class JDBCTemplate {
+	
+	public static Connection getConnection() {
+		
+		Connection conn = null;
+		
+		Properties prop = new Properties();
+	}
+}
